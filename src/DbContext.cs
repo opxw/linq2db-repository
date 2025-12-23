48c08f9db4b31265a1ea927b60999e0de72b0cc5
opx <pleasureunderpressure@gmail.com>
2025-12-23 18:02:05 +0700

    add DbContext base class to provide multiple db conn using classic style

---
+++ src/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using LinqToDB;
+using LinqToDB.Repository;
+
+namespace LinqToDb.Repository
+{
+	public class DbContext : DbContextRepository
+	{
+		public DbContext(string providerName, string connectionString) : base(providerName, connectionString)
+		{
+		}
+
+		public ITable<T> GetTable<T>() where T : class
+		{
+			return Connection.GetTable<T>();
+		}
+	}
+}
